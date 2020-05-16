7e6863acbf378e8c34e002344b03fda6d4f2a6a3
mdziardziel <mdziardziel@gmail.com>
2020-05-16 03:01:06 +0200

    add ingredients to new

--- assets/js/pages/recipe/new.page.js
+++ assets/js/pages/recipe/new.page.js
@@ -1,13 +1,30 @@
 var createRecipe = function() {
+  var ingredients = function(){
+    var ingredients = []
+    $('#ingredientForm').find('.input-group').each(function(index) {
+      var ingredient = {
+        name: $(this).find('#ingredient-name').val(),
+        quantity: $(this).find('#ingredient-quantity').val()
+      }
+      ingredients.push(ingredient)
+    })
+    return ingredients;
+  }
   $.ajax({
     url: '/api/v1/recipe/',
     type: 'POST',
     data: {
       title: $('#recipeTitleInput').val(),
-      description: $('#recipeDescriptionInput').val()
+      description: $('#recipeDescriptionInput').val(),
+      ingredients: ingredients()
     },
     success: function(result) {
       window.location = '/show/' + result.slug
     }
 });
 } 
+
+var addIngredient = function(){
+  input = $('<div class="input-group"><label>Name:</label> <input class="form-control" id="ingredient-name"><label> Quantity:</label> <input class="form-control" id="ingredient-quantity"></div>');
+  $('#ingredientForm').append(input);
+}
